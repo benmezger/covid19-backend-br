4f80fac996665508fbf2b46ba489291c3e21799a
Ben Mezger <me@benmezger.nl>
2020-03-21 20:09:13 -0300

    Feat: Add RuleCondition and LogicalCondition models

---
+++ src/rules/datatypes.py
@@ -0,0 +1,41 @@
+from enum import Enum
+
+
+class BaseEnum(Enum):
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name.title()) for key in cls]
+
+    def __str__(self):
+        return self.value
+
+    @classmethod
+    def has_value(cls, value):
+        return value.lower() in [key.value.lower() for key in cls]
+
+    @classmethod
+    def has_key(cls, key):
+        return key.lower() in [key.name.lower() for key in cls]
+
+
+class ConditionOperator(BaseEnum):
+    GREATER_THAN = (">", "gt")
+    GREATER_THAN_OR_EQUAL_TO = (">=", "gte")
+    LESS_THAN = ("<", "lt")
+    LESS_THAN_OR_EQUAL_TO = ("=<", "lte")
+
+    @classmethod
+    def choices(cls):
+        return [(key.value[1], key.value[0]) for key in cls]
+
+    @classmethod
+    def has_value(cls, value):
+        return value.lower() in [key.value[1].lower() for key in cls]
+
+
+class ConditionAttribute(BaseEnum):
+    AGE = "Idade"
+
+    @classmethod
+    def choices(cls):
+        return [(key.name.lower(), key.value) for key in cls]
--- src/rules/models.py
+++ src/rules/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django_extensions.db.models import TimeStampedModel
 
+from rules.datatypes import ConditionAttribute, ConditionOperator
+
 
 class Rule(TimeStampedModel):
     name = models.CharField(
@@ -14,3 +16,42 @@ class Rule(TimeStampedModel):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class RuleCondition(TimeStampedModel):
+    rule = models.ForeignKey(
+        "rules.Rule",
+        null=True,
+        verbose_name="Regra",
+        on_delete=models.SET_NULL,
+        related_name="conditions",
+    )
+
+
+class LogicalCondition(TimeStampedModel):
+    rule_condition = models.ForeignKey(
+        "rules.RuleCondition",
+        verbose_name="Condicao",
+        on_delete=models.CASCADE,
+        related_name="logical_conditions",
+    )
+
+    attribute = models.CharField(
+        max_length=10,
+        choices=ConditionAttribute.choices(),
+        null=False,
+        blank=False,
+        verbose_name="Atributo",
+    )
+
+    operator = models.CharField(
+        max_length=10,
+        choices=ConditionOperator.choices(),
+        null=False,
+        blank=False,
+        verbose_name="Operador",
+    )
+
+    value = models.CharField(
+        max_length=100, null=False, blank=False, verbose_name="Valor",
+    )
