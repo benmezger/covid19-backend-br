7518d4b36c83b1e5242e38bbe3fa2c31d954a18b
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-03-21 10:54:04 -0300

    Remove terraform folder

--- terraform/modules/iam/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "environment" {}
-variable "project_name" {}
-variable "region" {}
-variable "ecr_list" {
-  default = []
-  type = list(string)
-}
-variable app_bucket {}
--- terraform/modules/network/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "environment" {}
-variable "project_name" {}
-variable "region" {}
-variable "availability_zone_1" {}
-variable "availability_zone_2" {}
