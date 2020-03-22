9213b8c17b6ad02362c4577936be1221a07c7eb8
Ben Mezger <me@benmezger.nl>
2020-03-21 22:27:26 -0300

    Style: Black it

--- src/rules/apps.py
+++ src/rules/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class RulesConfig(AppConfig):
-    name = 'rules'
+    name = "rules"
--- src/rules/migrations/0001_initial.py
+++ src/rules/migrations/0001_initial.py
@@ -8,24 +8,41 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Rule',
+            name="Rule",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
-                ('name', models.CharField(max_length=255, verbose_name='Nome')),
-                ('message', models.CharField(max_length=255, verbose_name='Mensagem')),
-                ('enabled', models.BooleanField(default=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                (
+                    "created",
+                    django_extensions.db.fields.CreationDateTimeField(
+                        auto_now_add=True, verbose_name="created"
+                    ),
+                ),
+                (
+                    "modified",
+                    django_extensions.db.fields.ModificationDateTimeField(
+                        auto_now=True, verbose_name="modified"
+                    ),
+                ),
+                ("name", models.CharField(max_length=255, verbose_name="Nome")),
+                ("message", models.CharField(max_length=255, verbose_name="Mensagem")),
+                ("enabled", models.BooleanField(default=True)),
             ],
             options={
-                'ordering': ('-modified', '-created'),
-                'get_latest_by': 'modified',
-                'abstract': False,
+                "ordering": ("-modified", "-created"),
+                "get_latest_by": "modified",
+                "abstract": False,
             },
         ),
     ]
