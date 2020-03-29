159f9246f95b76f2270a5929e31b2889472d52c8
Rodolfo Lottin Pereira <rodolfolottin1@gmail.com>
2020-03-29 14:10:16 -0300

    feat: Add authentication to notification viewset

--- src/notification/tests/views/test_notification_viewset.py
+++ src/notification/tests/views/test_notification_viewset.py
@@ -10,7 +10,39 @@ def test_notification_create(client, db, make_person, make_rule):
     rule = make_rule()
 
     payload = {
-        "person_beacon_id": person.beacon_id,
+        "rule_id": rule.pk,
+        "delivered": True,
+        "read": False,
+    }
+
+    response = client.post(
+        reverse("notification:notification-list"),
+        data=payload,
+        content_type="application/json",
+        HTTP_AUTHORIZATION=f"PersonToken {person.token}"
+    )
+
+    assert Notification.objects.filter(person=person).count() == 1
+    assert response.status_code == 201
+
+    data = response.json()
+
+    assert data["created"]
+    assert data["id"]
+
+    assert data.items() >= {
+        "title": rule.name,
+        "message": rule.message,
+        "delivered": True,
+        "read": False,
+    }.items()
+
+
+def test_notification_create_unauthenticated(client, db, make_person, make_rule):
+    person = make_person()
+    rule = make_rule()
+
+    payload = {
         "rule_id": rule.pk,
         "delivered": True,
         "read": False,
@@ -22,10 +54,8 @@ def test_notification_create(client, db, make_person, make_rule):
         content_type="application/json",
     )
 
-    assert Notification.objects.count() == 1
-    notification = Notification.objects.last()
-
-    assert response.status_code == 201
+    assert Notification.objects.filter(person=person).count() == 0
+    assert response.status_code == 401
 
 
 def test_notification_update(client, db, make_notification):
@@ -37,9 +67,27 @@ def test_notification_update(client, db, make_notification):
         reverse("notification:notification-detail", kwargs={"pk": notification.pk}),
         data=payload,
         content_type="application/json",
+        HTTP_AUTHORIZATION=f"PersonToken {notification.person.token}"
     )
 
     notification = Notification.objects.get(pk=notification.pk)
     assert notification.read == True
 
     assert response.status_code == 200
+
+
+def test_notification_update_unauthenticated(client, db, make_notification):
+    notification = make_notification()
+
+    payload = {"read": True}
+
+    response = client.patch(
+        reverse("notification:notification-detail", kwargs={"pk": notification.pk}),
+        data=payload,
+        content_type="application/json",
+    )
+
+    notification = Notification.objects.get(pk=notification.pk)
+    assert notification.read == False
+
+    assert response.status_code == 401
